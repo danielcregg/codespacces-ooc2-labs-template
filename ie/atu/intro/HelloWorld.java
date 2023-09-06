d8cf8e7c48fa1123cda0e31fc254c70d9a883440
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-06 22:30:03 +0100

    Create HelloWorld.java

---
+++ ie/atu/intro/HelloWorld.java
@@ -0,0 +1,7 @@
+// This program prints 'Hello, World!' to the console.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints 'Hello, World!' to the console.
+        System.out.println("Hello, World!");
+    }
+}
