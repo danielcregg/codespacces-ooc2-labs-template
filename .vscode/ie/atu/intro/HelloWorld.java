acc595428bf1a62ed83574a4f977e4740d139e21
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-09-06 22:31:08 +0100

    Delete .vscode/ie/atu/intro directory

--- .vscode/ie/atu/intro/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-// This program prints 'Hello, World!' to the console.
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints 'Hello, World!' to the console.
-        System.out.println("Hello, World!");
-    }
-}
